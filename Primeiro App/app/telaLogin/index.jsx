da6e655a0b453ab381aeb56fa766dd8f5ee69307
schddan <danielschmitzsc@gmail.com>
2024-09-02 09:37:46 -0300

    Início da tela de login

---
+++ Primeiro App/app/telaLogin/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Text, TextInput, View, StyleSheet } from "react-native";
+
+export default Login = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Sign Up</Text>
+            <View>
+                <Text></Text>
+                <TextInput />
+                <Image src={uri = { icon }} />
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#FFFFFF",
+        width: '100%',
+        height: '100%',
+    }
+})
